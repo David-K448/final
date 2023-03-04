3fcd3ff30f56d5e6194c657dd65063e0990b5e1f
David-Killian <killianda36@gmail.com>
2023-03-04 13:29:07 -0500

    done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class App {
@@ -67,11 +70,163 @@ public class App {
                     System.out.println("Option 3 selected.\n");
                     runOPT(pageNumInput, refInput);
                     break;
+                case 4:
+                    System.out.println("Option 4 selected.\n");
+                    runNew(pageNumInput, refInput);
+                    break;
                 default:
                     System.out.println("Invalid input");
                     break;
             }
         }
+    }
+
+    public static int findSecondLargest(int[] arr) {
+        int[] sortedArr = Arrays.stream(arr)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+        return sortedArr[1];
+    }
+
+    public static int indexOf(int[] arr, int searchValue) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == searchValue) {
+                return i;
+            }
+        }
+        return -1; // not found
+    }
+
+    private static void runNew(int pageNumInput, long refInput) {
+        int currentFrame = 0;
+        int faultCount = 0;
+        int victim = -1;
+        int max = -1;
+        boolean fault;
+        int index;
+
+        ArrayList<Integer> refInputAL = new ArrayList<>();
+        String refString = String.valueOf(refInput);
+        int[] refInputArr = new int[String.valueOf(refInput).length()]; // initialize reference string array
+
+        for (int i = 0; i < refString.length(); i++) {
+            refInputArr[i] = Character.getNumericValue(refString.charAt(i));
+        } // fill the array
+
+        // System.out.println(Arrays.toString(refInputArr));
+
+        for (int i = 0; i < String.valueOf(refInput).length(); i++) {
+            refInputAL.add(i, Character.getNumericValue(refString.charAt(i)));
+        }
+        // previous block of code gets the refstring and puts it into both an array and
+        // array list for later use
+
+        // generate empty template table, print to console
+
+        ArrayList<Integer> memory = new ArrayList<>(pageNumInput);
+        ArrayList<Integer> refListAL = new ArrayList<>();
+
+        for (int i : refInputAL) {
+            refListAL.add(i);
+        }
+
+        String[][] outPutTable = makeTable(pageNumInput, refInputAL);
+
+        System.out.println("Current Table\n");
+        printTable(outPutTable);
+
+        System.out.print("\nPress Enter to continue... ");
+
+        promptEnterKey();
+
+        int[] arrayCounter = new int[pageNumInput]; // initialize array counter to number of pages
+        Arrays.fill(arrayCounter, 0); // initialize array with 0s
+
+        for (int i = 0; i < refInputAL.size(); i++) {
+            // checks if all pages have been filled yet
+            if (!memory.contains(refInputAL.get(i))) {
+                if (memory.size() < pageNumInput) {
+                    memory.add(currentFrame, refInputAL.get(i));
+                    refListAL.remove((Integer) refInputAL.get(i));
+                    arrayCounter[memory.size() - 1] = 1; // update counter for new page
+                    for (int k = 0; k <= pageNumInput - 1; k++) {
+                        if (k != memory.size() - 1 && k <= memory.size() - 1) {
+                            arrayCounter[k]++;
+                        }
+                    }
+                    ++currentFrame;
+                    fault = true;
+                    if (i >= pageNumInput) {
+                        faultCount++;
+                    }
+                } else { // Page fault on full memory, swap
+                    fault = true;
+                    if (i >= pageNumInput) {
+                        faultCount++;
+                    }
+                    // Step 1 remove current item.
+                    int temp = refListAL.get(0);
+                    refListAL.remove(0);
+
+                    int secondLargest = findSecondLargest(arrayCounter);
+                    // System.out.println("second="+secondLargest);
+                    // System.out.println("second index="+indexOf(arrayCounter, secondLargest));
+
+                    // memory.set(memory.indexOf(victim), temp); // Swap
+                    memory.set(indexOf(arrayCounter, secondLargest), temp); // Swap
+                    // System.out.println("max="+max);
+                    // System.out.println("tempindex="+memory.indexOf(max));
+                    // System.out.println("temp="+temp);
+                    // System.out.println("tempindex="+memory.indexOf(temp));
+                    // System.out.println("vict="+victim);
+                    // System.out.println("victind="+memory.indexOf(victim));
+                    // System.out.println(memory.toString());
+                    for (int k = 0; k <= pageNumInput - 1; k++) {
+                        if (k != memory.indexOf(temp)) {
+                            arrayCounter[k]++;
+                        } else {
+                            arrayCounter[k] = 1;
+                        }
+
+                    }
+
+                    max = -1; // Reset max
+                }
+            } else { // Memory contains reference
+                fault = false;
+                for (int k = 0; k <= pageNumInput - 1; k++) {
+                    if (k <= memory.size() - 1) {
+                        arrayCounter[k]++;
+                    }
+                }
+                refListAL.remove(0);
+            }
+
+            for (int j = 0; j < memory.size(); ++j) {
+                outPutTable[j + 1][i + 1] = String.valueOf(memory.get(j));
+            }
+
+            if (fault && i >= pageNumInput) {
+                outPutTable[pageNumInput + 1][i + 1] = "F";
+                if (victim != -1)
+                    outPutTable[pageNumInput + 2][i + 1] = String.valueOf(victim);
+            }
+
+            System.out.println("Current Table\n");
+            printTable(outPutTable);
+
+            System.out.println("\nArray Counter:");
+            System.out.println(Arrays.toString(arrayCounter));
+
+            System.out.print("\nPress Enter to continue... ");
+            promptEnterKey();
+        }
+
+        System.out.println("\nOPT  is now complete");
+        System.out.println("A total of " + faultCount + " faults occurred");
+
     }
 
     private static void runOPT(int pageNumInput, long refInput) {
@@ -81,15 +236,16 @@ public class App {
         int max = -1;
         boolean fault;
         int index;
+
         ArrayList<Integer> refInputAL = new ArrayList<>();
         String refString = String.valueOf(refInput);
-        int[] refInputArr = new int[String.valueOf(refInput).length()];
+        int[] refInputArr = new int[String.valueOf(refInput).length()]; // initialize reference string array
 
         for (int i = 0; i < refString.length(); i++) {
             refInputArr[i] = Character.getNumericValue(refString.charAt(i));
-        }
+        } // fill the array
 
-        System.out.println(Arrays.toString(refInputArr));
+        // System.out.println(Arrays.toString(refInputArr));
 
         for (int i = 0; i < String.valueOf(refInput).length(); i++) {
             refInputAL.add(i, Character.getNumericValue(refString.charAt(i)));
@@ -116,6 +272,7 @@ public class App {
         promptEnterKey();
 
         for (int i = 0; i < refInputAL.size(); i++) {
+            // checks if all pages have been filled yet
             if (!memory.contains(refInputAL.get(i))) {
                 if (memory.size() < pageNumInput) {
                     memory.add(currentFrame, refInputAL.get(i));
@@ -125,7 +282,6 @@ public class App {
                     if (i >= pageNumInput) {
                         faultCount++;
                     }
-
                 } else { // Page fault on full memory, swap
                     fault = true;
                     if (i >= pageNumInput) {
@@ -185,7 +341,6 @@ public class App {
         System.out.println("Press \"ENTER\" to continue...");
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
-        scanner.close();
     }
 
     private static void printTable(String[][] outPutTable) {
@@ -242,4 +397,4 @@ public class App {
 
 }
 
-// 2456751452365345356
+// 245675145236345356
